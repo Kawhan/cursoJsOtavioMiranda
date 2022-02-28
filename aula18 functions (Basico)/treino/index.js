d8228e0ad0561f28f6c55e5d2db8b965bba03da5
Kawhan <kawhanlaurindo@outlook.com>
2022-02-28 17:02:36 -0300

    commit_22_fev_2022

---
+++ aula18 functions (Basico)/treino/index.js
@@ -0,0 +1,16 @@
+const soma = (num1, num2) => {
+    return num1 + num2;
+};
+
+const mult = (num1, num2) => {
+    return num1 + num2;
+}
+
+const divisao = function (num1, num2) {
+    return num1 / num2;
+};
+
+
+console.log(soma(2,2));
+console.log(mult(2, 2));
+console.log(divisao(2, 2));
